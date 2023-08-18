0d15572bdc23121db5050d8fce3d5286a6f9e37f
CryptoAutistic80 <120666391+CryptoAutistic80@users.noreply.github.com>
2023-08-18 19:08:33 +0000

    path change for db

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-##Config
+## Guild and Channel Config
 
 TRANSLATE_CONFIG = {
-    1081674998256775239: {
-        'channels': [1112510368879743146]        
+    1081674998256775239: {                                                ##SShift Development Server
+        'channels': [1112510368879743146]                                 ##TEXT CHANNEL
     },
-    1098355558022656091: {
-        'channels': [1098355558538559564, 1101204273339056139]
+    1098355558022656091: {                                                ##SShift DAO
+        'channels': [1098355558538559564, 1101204273339056139]            ##General, Holders
     }
 }
 
