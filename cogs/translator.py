80773e3e259885a4be913ff5892894ede50f7524
CryptoAutistic80 <120666391+CryptoAutistic80@users.noreply.github.com>
2023-08-18 18:59:28 +0000

    Improved discord UI

--- cogs/translator.py
+++ cogs/translator.py
@@ -16,7 +16,7 @@ def is_english(text):
 
 def preprocess_message(text):
     # Remove mentions
-    cleaned_text = re.sub(r'@\w+', '', text)
+    cleaned_text = re.sub(r'@\\w+', '', text)
     return cleaned_text.strip()
 
 def should_translate(text):
@@ -28,25 +28,25 @@ def should_translate(text):
 
     # Check for non-Latin scripts
     non_latin_patterns = [
-        r'[\u0600-\u06FF]',  # Arabic
-        r'[\u0980-\u09FF]',  # Bengali
-        r'[\u4E00-\u9FFF\U00020000-\U0002A6DF]',  # Chinese
-        r'[\u0400-\u04FF]',  # Cyrillic
-        r'[\u0900-\u097F]',  # Devanagari
-        r'[\u0370-\u03FF]',  # Greek
-        r'[\u0A80-\u0AFF]',  # Gujarati
-        r'[\u0A00-\u0A7F]',  # Gurmukhi
-        r'[\u0590-\u05FF]',  # Hebrew
-        r'[\u3040-\u30ff\u3400-\u4DBF]',  # Japanese
-        r'[\u0C80-\u0CFF]',  # Kannada
-        r'[\uAC00-\uD7AF]',  # Korean
-        r'[\u0D00-\u0D7F]',  # Malayalam
-        r'[\u0B00-\u0B7F]',  # Oriya (Odia)
-        r'[\u0D80-\u0DFF]',  # Sinhala
-        r'[\u0B80-\u0BFF]',  # Tamil
-        r'[\u0C00-\u0C7F]',  # Telugu
-        r'[\u0E00-\u0E7F]',  # Thai
-        r'[\u0F00-\u0FFF]'   # Tibetan
+        r'[\\u0600-\\u06FF]',  # Arabic
+        r'[\\u0980-\\u09FF]',  # Bengali
+        r'[\\u4E00-\\u9FFF\\U00020000-\\U0002A6DF]',  # Chinese
+        r'[\\u0400-\\u04FF]',  # Cyrillic
+        r'[\\u0900-\\u097F]',  # Devanagari
+        r'[\\u0370-\\u03FF]',  # Greek
+        r'[\\u0A80-\\u0AFF]',  # Gujarati
+        r'[\\u0A00-\\u0A7F]',  # Gurmukhi
+        r'[\\u0590-\\u05FF]',  # Hebrew
+        r'[\\u3040-\\u30ff\\u3400-\\u4DBF]',  # Japanese
+        r'[\\u0C80-\\u0CFF]',  # Kannada
+        r'[\\uAC00-\\uD7AF]',  # Korean
+        r'[\\u0D00-\\u0D7F]',  # Malayalam
+        r'[\\u0B00-\\u0B7F]',  # Oriya (Odia)
+        r'[\\u0D80-\\u0DFF]',  # Sinhala
+        r'[\\u0B80-\\u0BFF]',  # Tamil
+        r'[\\u0C00-\\u0C7F]',  # Telugu
+        r'[\\u0E00-\\u0E7F]',  # Thai
+        r'[\\u0F00-\\u0FFF]'   # Tibetan
     ]
     for pattern in non_latin_patterns:
         if re.search(pattern, cleaned_text):
@@ -57,7 +57,7 @@ def should_translate(text):
         return False
     
     # Check for URLs on cleaned text
-    url_regex = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    url_regex = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\\\(\\\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
     if re.search(url_regex, cleaned_text):
         return False
 
@@ -80,7 +80,7 @@ class TranslationView(nextcord.ui.View):
     def __init__(self, cog, message_id, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.cog = cog
-        self.add_item(TranslationButton(message_id, label="View Translation", style=nextcord.ButtonStyle.grey))
+        self.add_item(TranslationButton(message_id, label="Translating...", style=nextcord.ButtonStyle.grey, disabled=True))
 
 class TranslationCog(commands.Cog):
     def __init__(self, bot):
@@ -90,7 +90,7 @@ class TranslationCog(commands.Cog):
 
     @tasks.loop(hours=12)
     async def clean_translations(self):
-        """Cleanup task to remove translations older than 12 hours."""
+        # Cleanup task to remove translations older than 12 hours.
         await delete_old_translations()
 
     @commands.Cog.listener()
@@ -102,6 +102,10 @@ class TranslationCog(commands.Cog):
         # Check if the message's guild and channel IDs match the TRANSLATE_CONFIG
         if guild_id in TRANSLATE_CONFIG and channel_id in TRANSLATE_CONFIG[guild_id]['channels'] and not message.author.bot:
             if should_translate(message.content):
+                # Display a non-functional button with label "Translating..."
+                dummy_view = TranslationView(self, message_id=message.id, timeout=None)
+                dummy_message = await message.channel.send("", view=dummy_view)
+
                 chat_message = [{"role": "user", "content": f"Translate the following to English: '{message.content}'"}]
                 response = openai.ChatCompletion.create(
                     model="gpt-3.5-turbo",
@@ -115,7 +119,13 @@ class TranslationCog(commands.Cog):
                 
                 # Send an empty message with just the 'Translation' button below the original message
                 view = TranslationView(self, message_id=message.id, timeout=None)
-                await message.channel.send("", view=view)
+                
+                # Edit the dummy button's label to "View Translation" and activate its functionality
+                dummy_view.clear_items()
+                dummy_view.add_item(TranslationButton(message_id=message.id, label="View Translation", style=nextcord.ButtonStyle.grey))
+                await dummy_message.edit(view=dummy_view)
+
+                # Removed the original button creation line
 
     def cog_unload(self):
         self.cleanup_task.cancel()
