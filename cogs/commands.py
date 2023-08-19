755a3dee28b88af76e9944552e2ac41339651416
CryptoAutistic80 <120666391+CryptoAutistic80@users.noreply.github.com>
2023-08-19 23:08:38 +0000

    Tired now goodnight, yes i am unprofessional

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,13 +1,18 @@
 import nextcord
+import nextcord
+import logging
 from nextcord.ext import commands
 from database.database_manager import retrieve_translation_by_original_message_id
 
 class CommandsCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        logging.info("CommandsCog initialized")
 
     @commands.command(name="fetch", help="Fetch the translation for a replied message")
-    async def fetch(self, ctx):
+    async def fetch_command(self, ctx):
+        """Fetch the translation for a replied message using traditional command"""
+        logging.info(f"!fetch command invoked by {ctx.author.name} ({ctx.author.id})")
         # Check if the context is in reply to an existing message
         if ctx.message.reference:
             original_message_id = ctx.message.reference.message_id
@@ -16,7 +21,33 @@ class CommandsCog(commands.Cog):
             # If a translation exists, send it
             if translation:
                 await ctx.send(translation)
+            else:
+                await ctx.send(f"No translation found for message ID {original_message_id}")
+        else:
+            await ctx.send("Please reply to a message to fetch its translation.")
+
+    @nextcord.slash_command(name="gettrans", description="Fetch the translation for a replied message")
+    async def gettrans(self, interaction: nextcord.Interaction, 
+                  option: str = nextcord.SlashOption(
+                      choices={"Translation": "translation"},
+                      description="Select an option to fetch"),
+                  ):
+        """Fetch the translation for a replied message using slash command"""
+        logging.info(f"/gettrans command invoked by {interaction.user.name} ({interaction.user.id})")
+        # Check if the interaction is in reply to an existing message
+        if interaction.message.reference:
+            original_message_id = interaction.message.reference.message_id
+            # Fetch the translation from the database
+            retrieved_translation = await retrieve_translation_by_original_message_id(original_message_id)
+            # If a translation exists, send it
+            if retrieved_translation:
+                await interaction.response.send_message(retrieved_translation, ephemeral=True)
+            else:
+                await interaction.response.send_message(f"No translation found for message ID {original_message_id}", ephemeral=True)
+        else:
+            await interaction.response.send_message("Please reply to a message to fetch its translation.", ephemeral=True)
 
 def setup(bot):
     bot.add_cog(CommandsCog(bot))
-    print("Commands cog loaded")
+    print("Command Cog loaded")
+    logging.info("CommandsCog has been loaded")
--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import os
 import nextcord
+import logging
+import logging.handlers
 from nextcord.ext import commands
 
 # Import the start_server function from your FastAPI file and the TRANSLATE_CONFIG from config.py
 from server import start_server
 from config import TRANSLATE_CONFIG
 from database.database_manager import initialize_db
+
+# Logging setup
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.handlers.RotatingFileHandler(filename='discord.log', encoding='utf-8', maxBytes=10**7, backupCount=5)
+console_handler = logging.StreamHandler()
+fmt = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+handler.setFormatter(fmt)
+console_handler.setFormatter(fmt)
+logger.addHandler(handler)
+logger.addHandler(console_handler)
 
 # Create an Intents object with all intents enabled
 intents = nextcord.Intents.all()
@@ -15,21 +28,31 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}!')
+    logger.info(f'Logged in as {bot.user.name}!')
     await initialize_db()
     
-    # Load cogs based on TRANSLATE_CONFIG
+    logger.info("About to load extensions.")
+    
+    loaded_cogs = set()  # To keep track of which cogs are already loaded
+
     for guild in bot.guilds:
-        if guild.id in TRANSLATE_CONFIG:
+        logger.info(f"Bot is in guild: {guild.name} ({guild.id})")
+        if guild.id in TRANSLATE_CONFIG and 'cogs.translator' not in loaded_cogs:
+            logger.info(f"Loading extensions for guild: {guild.name} ({guild.id})")
             bot.load_extension('cogs.translator')
+            loaded_cogs.add('cogs.translator')
+
+        if guild.id in TRANSLATE_CONFIG and 'cogs.commands' not in loaded_cogs:
             bot.load_extension('cogs.commands')
-            break  # Exit loop once the translator cog is loaded
+            loaded_cogs.add('cogs.commands')
 
 # Start the FastAPI server to keep the Replit project awake
 start_server()
 
 # Run the bot
 bot.run(os.getenv('DISCORD_TOKEN'))
+
+
 
 
 
