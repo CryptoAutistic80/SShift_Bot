adf96d633b1add41534d286df0756ea748cd8562
CryptoAutistic80 <120666391+CryptoAutistic80@users.noreply.github.com>
2023-09-03 18:42:04 +0000

    guild membership table added to db with appropriate functions

--- database/database_manager.py
+++ database/database_manager.py
@@ -1,7 +1,7 @@
 import aiosqlite
 
 # Define the path for the SQLite database
-db_path = "database/translations_database.db"
+db_path = "database/database.db"
 
 async def initialize_db():
     """Initialize the database and create tables if they don't exist."""
@@ -17,6 +17,15 @@ async def initialize_db():
                     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
                     original_message_id TEXT NOT NULL,
                     PRIMARY KEY (guild_id, id)
+                );
+            """)
+            
+            await cursor.execute("""
+                CREATE TABLE IF NOT EXISTS guild_memberships (
+                    guild_id TEXT PRIMARY KEY NOT NULL,
+                    membership_type TEXT NOT NULL,
+                    expiry_date DATETIME NOT NULL,
+                    subscription_active BOOLEAN NOT NULL
                 );
             """)
             await db.commit()
@@ -64,6 +73,69 @@ async def retrieve_translation_by_original_message_id(guild_id, original_message
             await cursor.execute("SELECT translation FROM translations WHERE guild_id = ? AND original_message_id = ?", (guild_id, original_message_id))
             translation = await cursor.fetchone()
             return translation[0] if translation else None
+    except aiosqlite.Error as e:
+        print(f"Database error: {e}")
+
+async def add_guild(guild_id, membership_type, expiry_date, subscription_active):
+    """Insert a new guild membership into the database."""
+    try:
+        async with aiosqlite.connect(db_path) as db:
+            cursor = await db.cursor()
+            await cursor.execute(
+                "INSERT INTO guild_memberships (guild_id, membership_type, expiry_date, subscription_active) VALUES (?, ?, ?, ?)", 
+                (guild_id, membership_type, expiry_date, subscription_active))
+            await db.commit()
+    except aiosqlite.Error as e:
+        print(f"Database error: {e}")
+
+async def remove_guild(guild_id):
+    """Remove a guild membership from the database based on the guild_id."""
+    try:
+        async with aiosqlite.connect(db_path) as db:
+            cursor = await db.cursor()
+            await cursor.execute("DELETE FROM guild_memberships WHERE guild_id = ?", (guild_id,))
+            await db.commit()
+    except aiosqlite.Error as e:
+        print(f"Database error: {e}")
+
+async def edit_guild(guild_id, membership_type, expiry_date, subscription_active):
+    """Update an existing guild membership's details based on the guild_id."""
+    try:
+        async with aiosqlite.connect(db_path) as db:
+            cursor = await db.cursor()
+            await cursor.execute(
+                "UPDATE guild_memberships SET membership_type = ?, expiry_date = ?, subscription_active = ? WHERE guild_id = ?", 
+                (membership_type, expiry_date, subscription_active, guild_id))
+            await db.commit()
+    except aiosqlite.Error as e:
+        print(f"Database error: {e}")
+
+async def retrieve_guild_membership(guild_id):
+    """Retrieve all guild membership details from the database based on the guild_id."""
+    try:
+        async with aiosqlite.connect(db_path) as db:
+            cursor = await db.cursor()
+            await cursor.execute("SELECT * FROM guild_memberships WHERE guild_id = ?", (guild_id,))
+            membership_details = await cursor.fetchone()
+            if membership_details:
+                return {
+                    "guild_id": membership_details[0],
+                    "membership_type": membership_details[1],
+                    "expiry_date": membership_details[2],
+                    "subscription_active": membership_details[3]
+                }
+            return None
+    except aiosqlite.Error as e:
+        print(f"Database error: {e}")
+
+async def retrieve_active_subscriptions():
+    """Retrieve all guild_ids with active subscriptions from the database."""
+    try:
+        async with aiosqlite.connect(db_path) as db:
+            cursor = await db.cursor()
+            await cursor.execute("SELECT guild_id FROM guild_memberships WHERE subscription_active = 1")
+            active_guilds = await cursor.fetchall()
+            return [guild[0] for guild in active_guilds]
     except aiosqlite.Error as e:
         print(f"Database error: {e}")
 
